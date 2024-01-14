facbfea347ba43e0e75c502867034a49e20149d0
quollveth <klaus.marrer@outlook.com>
2024-01-14 17:03:06 -0300

    added some icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import IconReact from "./components/lib/IconReact";
+import IconBrandFacebook from "./components/lib/IconBrandFacebook";
+import IconBrandInstagram from "./components/lib/IconBrandInstagram";
+import IconBrandTwitter from "./components/lib/IconBrandTwitter";
+
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
-  )
-}
+  <>
 
-export default App
+  <header className="header p-2 bg-gray-800 grid grid-cols-1 grid-rows-2 gap-2">
+    <nav id="topbar" className="Navbar topbar container mx-auto flex items-center justify-between">
+      <div className="flex items-center space-x-4">
+        <a href="#" className="px-4 py-2 transition duration-300 hover:text-yellow-200">Categorias</a>
+        <a href="#" className="px-4 py-2 transition duration-300 hover:text-yellow-200">Sobre nós</a>
+        <a href="#" className="px-4 py-2 transition duration-300 hover:text-yellow-200">Entrega</a>
+      </div>
+      <nav id="socialBar" className="flex items-center space-x-4">
+        <span>Siga-nos no</span>
+        <a href="#" className="text-white hover:text-blue-400"> <IconBrandTwitter/></a>
+        <a href="#" className="text-white hover:text-blue-600"> <IconBrandFacebook/></a>
+        <a href="#" className="text-white hover:text-pink-500"> <IconBrandInstagram/></a>
+      </nav>
+    </nav>
+    <nav id="topbarBtm" className="Navbar topbar container mx-auto flex flex-start justify-between">
+      <a href="/"> <IconReact/></a>
+      <a href="#" className="px-4 py-2">SEARCH</a>
+      <a href="#" className="px-4 py-2">CART</a>
+    </nav>
+  </header>
+
+  </>
+  );
+};
+
+export default App;
---
+++ src/components/lib/IconBrandInstagram.jsx
@@ -0,0 +1,25 @@
+// icon:brand-instagram | Tabler Icons https://tablericons.com/ | Csaba Kissi
+import * as React from "react";
+
+function IconBrandInstagram(props) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M8 4 H16 A4 4 0 0 1 20 8 V16 A4 4 0 0 1 16 20 H8 A4 4 0 0 1 4 16 V8 A4 4 0 0 1 8 4 z" />
+      <path d="M15 12 A3 3 0 0 1 12 15 A3 3 0 0 1 9 12 A3 3 0 0 1 15 12 z" />
+      <path d="M16.5 7.5v.001" />
+    </svg>
+  );
+}
+
+export default IconBrandInstagram;
