ec24806092e18e26bf43449ffa7e46c24179c3f2
quollveth <klaus.marrer@outlook.com>
2024-01-16 09:42:41 -0300

    hover works

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -1,38 +1,79 @@
-import { Popover } from '@headlessui/react';
-import { useState } from 'react';
-import { usePopper } from 'react-popper';
-import './CartMenu.css';
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment, useRef } from "react";
 
-function CartMenu(props){
-;
-    const [referenceElement, setReferenceElement] = useState(null);
-    const [popperElement, setPopperElement] = useState(null);
-    const [arrowElement, setArrowElement] = useState(null);
-    const { styles, attributes } = usePopper(referenceElement, popperElement, {
-      modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
-    });
+export default function CartMenu(props) {
+  const ButtonRef = useRef(null);
+  const timeoutDuration = 200;
+  let timeout;
 
-    return (
-        <Popover>
-            <Popover.Button
-                id="CartButton"
-                ref={setReferenceElement}
-            >{props.button}
-            </Popover.Button>
-            <Popover.Panel
-                id="CartMenu"
-                className="z-10"
-                ref={setPopperElement}
-                style={styles.popper}
-                {...attributes.popper}
+  const closePopover = () => {
+    return ButtonRef.current?.dispatchEvent(
+      new KeyboardEvent("keydown", {
+        key: "Escape",
+        bubbles: true,
+        cancelable: true
+      })
+    );
+  };
+
+  const onMouseEnter = (open) => {
+    clearTimeout(timeout)
+    if (open) return;
+    return ButtonRef.current?.click();
+  };
+
+  const onMouseLeave = (open) => {
+    if (!open) return;
+    timeout = setTimeout(() => closePopover(), timeoutDuration);
+  };
+
+  return (
+    <div>
+      <Popover className="relative">
+        {({ open }) => {
+          return (
+            <>
+              <div onMouseLeave={onMouseLeave.bind(null, open)}>
+                <Popover.Button
+                  id="CartButton"
+                  ref={ButtonRef}
+                  className={`
+                  ${open ? "" : "text-opacity-90"}`}
+                  onMouseEnter={onMouseEnter.bind(null, open)}
+                  onMouseLeave={onMouseLeave.bind(null, open)}
                 >
-                <div id="CartArrow" ref={setArrowElement} style={styles.arrow}/>
-                <div className="w-32 h-32">
-                        <span>Carrinho vazio</span>
-                </div>
-            </Popover.Panel>   
-        </Popover>
-    )
+                  {props.button}
+                </Popover.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel 
+                    id="CartMenu"
+                    className="absolute"
+                    >
+                    <div
+                      className="rounded-lg w-32 h-32 z-10"
+                      onMouseEnter={onMouseEnter.bind(null, open)}
+                      onMouseLeave={onMouseLeave.bind(null, open)}
+                    >
+                        <div id="CartArrow"/>
+                        <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                        {/* CONTENT HERE */}
+                        </div>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </div>
+            </>
+          )
+        }}
+      </Popover>
+    </div>
+  )
 }
-
-export default CartMenu;
